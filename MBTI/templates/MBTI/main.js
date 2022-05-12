45d6aa395f5b289153938c0b3ef329cae7f14bd5
terrykim96 <terrykim96@naver.com>
2022-05-13 02:12:29 +0900

    Change Django to JS

--- MBTI/templates/MBTI/main.js
+++
@@ -1,9 +0,0 @@
-const testDict = {'EI':0, 'NS':0, 'TF':0, 'PJ':0}
-
-const testList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-localStorage.setItem('testDict', JSON.stringify(testDict));
-
-const retrievedDict = localStorage.getItem('testDict');
-
-console.log('retrievedDict: ', JSON.parse(retrievedDict));

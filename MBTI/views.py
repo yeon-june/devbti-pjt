1ffc262c7c4e82bcc7eaf433a6307178fcc20c11
Yeon <theresjahn@gmail.com>
2022-05-18 21:18:23 +0900

    error modification1

--- MBTI/views.py
+++ MBTI/views.py
@@ -4,9 +4,11 @@ from .models import Mbti, BaseInfo, Howto, Question
 
 # Create your views here.
 
-flag = 0
+
 
 def main_page(request):
+    global flag
+    flag = 0
     # 비정상적인 접근 막기! (초기값 다르게,, 전체 문항 안고르면 메인으로 돌려버리기)
     mbties = Mbti.objects.all()
     viewers = 0
